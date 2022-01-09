e285d8a02b0c8299c3bae9ac236dffd2fb8b5dfc
Gyuri21 <gyuri21hegedus@gmail.com>
2022-01-09 13:25:22 +0100

    info gomb mukodik

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 /*
  * File: app.js
  * Authors: Olah Janos Gergely & Hegedus Gyorgy
- * Copyright: 2021, Olah Janos Gergely & Hegedus Gyorgy
+ * Copyright: 2022, Olah Janos Gergely & Hegedus Gyorgy
  * Group: Szoft II/N
  * Date: 2022-01-03
  * Github: https://github.com/olahjg & https://github.com/Gyuri21
@@ -9,13 +9,14 @@
  */
 import { StatusBar } from 'expo-status-bar';
 import React, {useState}from 'react';
-import {TouchableHighlight, ImageBackground, StyleSheet, Text, View, TextInput} from 'react-native';
+import {TouchableHighlight, ImageBackground, StyleSheet, Text, View, TextInput,Button} from 'react-native';
 
 const image = { uri: "https://www.patternpictures.com/wp-content/uploads/Geometric-pentagon-shapes-sublte-white-background-seamless-pattern-patternpictures-0220.png" };
 
 export default function App() {
   const[sidelenght,setSidelenght] = useState();
   const[area,setArea] = useState();
+  const [shouldShow, setShouldShow] = useState(true);
 
   function calcArea(){
       let result = Math.pow(sidelenght,2)*Math.sqrt(25+10*Math.sqrt(5))/4;
@@ -26,7 +27,17 @@ export default function App() {
   return (
     
       <ImageBackground source={image} resizeMode="cover" style={styles.image}>
-      <Text style={styles.texts}>Pentagon</Text>
+      <Text style={styles.text1}>Pentagon</Text>
+      <Button
+          title="info"
+          color="blue"
+          onPress={() => setShouldShow(!shouldShow)}
+        />
+        {}
+        {shouldShow ?
+        (
+          <Text style={styles.text2}>A program egy pentagon teterületének a kiszámitására hivatott. Hegedűs György / Oláh János Gergely 2022-01-03</Text>
+        ) : null}
       <TextInput placeholder = "Oldalhosszusag: " style = {styles.input} onChangeText={sidelenght => setSidelenght(sidelenght)}/>
       <TouchableHighlight
           style={styles.runButton}
@@ -37,6 +48,9 @@ export default function App() {
       <Text style={styles.results}>Terület</Text>
       <Text>{area}</Text>
       <StatusBar style="auto" />
+
+
+      
       </ImageBackground>
   );
 }
@@ -58,10 +72,16 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     textAlign: 'center'
   },
-  texts:{
+  text1:{
     fontSize: 50,
     fontWeight: 'bold',
     color: 'blue'
+  },
+  text2:{
+    fontSize: 15,
+    fontWeight: 'normal',
+    color: 'blue',
+    textAlign :'center'
   },
   results:{
     fontSize: 30,
